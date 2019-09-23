905c147eb21a73c58a9f5d3a0bef36cb95c31c78
Marc-Steffen Kaesz <mskaesz@googlemail.com>
2019-09-23 18:59:26 +0200

    clean up

--- clearlinux-build-upcloud-from-custom-template/scripts/prepare-custom-template.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-swupd bundle-add clr-installer
-
-curl -O https://raw.githubusercontent.com/mkaesz/control_plane/master/clearlinux/scripts/kvm-legacy.yaml
-
-clr-installer -c kvm-legacy.yaml
-
-dd if=kvm-legacy.img of=/dev/vdb bs=16M oflag=direct
-
--- clearlinux-build-upcloud-from-custom-template/scripts/prepare_custom_template_based_on_default_kvm_legacy_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -o kvm-legacy.img.xz -O https://cdn.download.clearlinux.org/image/$(curl https://cdn.download.clearlinux.org/image/latest-images | grep '[0-9]'-kvm-legacy'\.')
-#curl https://cdn.download.clearlinux.org/image/latest-images
-
-unxz kvm-legacy.img.xz
-
-dd if=kvm-legacy.img of=/dev/vdb bs=16M oflag=direct
-
